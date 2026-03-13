689740f22868f8665bf21c6a946bb07653b7945b
Stanley <admin@gaboomholding.com>
2026-03-13 03:13:53 -0400

    Serve MEDIA_ROOT via WhiteNoise in prod

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,7 +1,11 @@
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=str(settings.STATIC_ROOT))
+application.add_files(str(settings.MEDIA_ROOT), prefix="media/")
