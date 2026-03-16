81325faeec641362637819ee80ce25e40b9fe8bc
Stanley <admin@gaboomholding.com>
2026-03-16 08:39:47 -0400

    Temporarily force DEBUG to diagnose login 500

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-insecure-change-me")
-DEBUG = os.environ.get("DJANGO_DEBUG", "1") == "1"
+DEBUG = True
 
 APP_NAME = "Gaboom DriveOS"
 
